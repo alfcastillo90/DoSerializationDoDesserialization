9a1109d01aea2ea79df245e82d439550e6a6ea1d
alfcastillo90 <cacr1990@gmail.com>
2018-05-01 17:50:48 -0300

    primer commit

---
+++ Rocket.cs
@@ -0,0 +1,8 @@
+namespace SeDes {
+    public class Rocket{
+        public int ID {set;get;}
+        public string Builder {get;set;}
+        public string Target {get;set;}
+        public double Speed {get;set;}
+    }
+}
