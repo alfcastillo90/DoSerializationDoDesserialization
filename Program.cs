97c2839255a69f6bdcf3a5ff3f66f6d4ced1bb90
alfcastillo90 <cacr1990@gmail.com>
2018-05-01 17:55:28 -0300

    se agrego un metodo que realiza serializaciones

--- Program.cs
+++ Program.cs
@@ -1,9 +1,19 @@
 ﻿using System;
-
+using Newtonsoft.Json;
 namespace SeDes
 {
     class Program
     {
+        public static string DoSerialization(){
+            Rocket[] rockets = {
+                new Rocket{ ID = 0, Builder = "NASA", Target = "Moon", Speed=7.8},
+                new Rocket{ ID = 1, Builder = "NASA", Target = "Mars", Speed=10.9},
+                new Rocket{ ID = 2, Builder = "NASA", Target = "Kepler-452b", Speed=42.1},
+                new Rocket{ ID = 3, Builder = "NASA", Target = "N/A", Speed=0}
+            }; 
+            var json = JsonConvert.SerializeObject(rockets);
+            return json;
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
